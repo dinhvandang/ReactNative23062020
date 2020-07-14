0a3b02ec0ac0e08db825688320831c619b8401a9
dinhvandang <dinhvandang.dev@gmail.com>
2020-07-14 21:03:26 +0700

    bt1

--- App.js
+++ App.js
@@ -1,78 +1,155 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
+import Main from './src/screen';
 
 export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
-        {/* <View
-          // eslint-disable-next-line react-native/no-inline-styles
+        <View
           style={{
+            backgroundColor: '#f20',
             flex: 1,
             flexDirection: 'column',
-            justifyContent: 'flex-start',
           }}>
-          <Text style={{backgroundColor: 'red', flex: 1, textAlign: 'center'}}>
-            A
-          </Text>
-          <Text style={{backgroundColor: 'blue', flex: 1, textAlign: 'center'}}>
-            B
-          </Text>
-          <Text style={{backgroundColor: 'gray', flex: 1, textAlign: 'center'}}>
-            C
-          </Text>
-          <Text style={{backgroundColor: 'yellow', flex: 1, textAlign: 'center'}}>
-            D
-          </Text>
+          <View
+            style={{
+              backgroundColor: 'red',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>A</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'green',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>B</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'blue',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>C</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'orange',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>D</Text>
+          </View>
         </View>
-
         <View
-          // eslint-disable-next-line react-native/no-inline-styles
           style={{
+            backgroundColor: 'yellow',
             flex: 1,
             flexDirection: 'row',
-            justifyContent: 'flex-end',
           }}>
-          <Text style={{backgroundColor: 'blue', flex: 1, textAlign: 'center'}}>
-            A
-          </Text>
-          <Text style={{backgroundColor: 'red', flex: 1, textAlign: 'center'}}>
-            B
-          </Text>
-          <Text style={{backgroundColor: 'gray', flex: 1, textAlign: 'center'}}>
-            C
-          </Text>
-          <Text
-            style={{backgroundColor: 'orange', flex: 1, textAlign: 'center'}}>
-            D
-          </Text>
-        </View> */}
-        <View
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{
-            flex: 0.1,
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-          }}>
-          <Text style={{backgroundColor: 'red', flex: 1, textAlign: 'center'}}>
-            1
-          </Text>
-        </View>
-
-        <View
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{
-            flex: 0.1,
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-          }}>
-          <Text
-            style={{backgroundColor: 'yellow', flex: 1, textAlign: 'center'}}>
-            2
-          </Text>
+          <View
+            style={{
+              backgroundColor: 'red',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>A</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'green',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>B</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'blue',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>C</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'orange',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>D</Text>
+          </View>
         </View>
       </SafeAreaView>
-    );
-  }
+
+<SafeAreaView style={styles.container}>
+<View style={styles.flexBoxColum}>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Red
+  </Text>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Green
+  </Text>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Blue
+  </Text>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Orange
+  </Text>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Gray
+  </Text>
+  <Text 
+    style={[
+      styles.textStyle,
+      {backgroundColor : `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}]}>
+      Pink
+  </Text>
+</View>
+</SafeAreaView>
+)
 }
+}
+
+
+const styles = StyleSheet.create({
+container : {
+flex : 1,
+justifyContent: 'center',
+alignItems : 'center'
+},
+textStyle: {
+color : 'black',
+fontSize : 20,
+textAlign: 'left'
+}
+
+}) 
